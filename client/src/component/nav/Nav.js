4cd400615edd4f81e16bd4e7edfc8bebd9fa7de2
Anya <makarobaanya@yandex.ru>
2022-03-27 23:45:04 +0300

    поиск по имени

--- client/src/component/nav/Nav.js
+++ client/src/component/nav/Nav.js
@@ -1,20 +1,44 @@
 import React from 'react';
 import { useSelector,useDispatch  } from 'react-redux';
 import { logoutUserAC } from '../../redux/actionCreators/userAC'
+import Search from '../Search/Search';
 
 function Nav() {
   const dispatch = useDispatch()
   const {user} = useSelector(state=>state.userReducer)
   return (
-    <nav>
-    <div className="nav-wrapper teal lighten-2">
-      {user.length ? <><a className="brand-logo">Список контактов</a>
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li onClick={() =>dispatch(logoutUserAC())}><a>Выйти</a></li>
-      </ul></> : <a href='/' className="brand-logo">Войти в телефонную книжку</a>}
-     
+
+    <nav width="650">
+    <div className="nav-wrapper light-blue darken-4">
+    {user.length 
+      ?
+       <><div className="brand-logo">Список контактов</div>
+      <ul className="right hide-on-med-and-down">
+     <li><Search/></li> 
+        <li onClick={() =>dispatch(logoutUserAC())}><a classNameName="right hide-on-med-and-down">Выйти</a></li>
+      </ul></> 
+      : 
+      <a href='/' className="brand-logo">Войти в телефонную книжку</a>}
     </div>
   </nav>
+
+
+
+
+  //   <nav>
+  //   <div className="row teal lighten-2">
+  //     {user.length 
+  //     ?
+  //      <><a className="col s12 m4 l2">Список контактов</a>
+  //     <li className="col s12 m4 l8"><Search/></li> 
+  //     <ul id="nav-mobile" className="col s12 m4 l2">
+  //       <li onClick={() =>dispatch(logoutUserAC())}><a>Выйти</a></li>
+  //     </ul></> 
+  //     : 
+  //     <a href='/' className="brand-logo">Войти в телефонную книжку</a>}
+     
+  //   </div>
+  // </nav>
   );
 }
 
--- client/src/redux/reducers/contactReduser.js
+++ client/src/redux/reducers/contactReduser.js
@@ -1,18 +1,20 @@
 import { contactAT } from '../actionType/contactAT'
 
-const initialState = { contacts: [{ id: 1, name: 'Александр', tel: '8987587456', email: 'alex@alex.ru', statusChange: false }] };
+const model ={ id: 1, name: 'Александр', tel: '8987587456', email: 'alex@alex.ru', statusChange: false }
+const initialState = { serchContacts:[model],contacts: [model] };
 
 function contactReduser(state = initialState, action) {
 
   switch (action.type) {
     case contactAT.CONTACT_ADD:
-      return { ...state, contacts: [...state.contacts, { id: new Date(), ...action.payload, statusChange: false }] }
+      return { ...state, serchContacts: [...state.serchContacts, { id: new Date(), ...action.payload, statusChange: false }],
+      contacts: [...state.contacts, { id: new Date(), ...action.payload, statusChange: false }] }
 
     case contactAT.CONTACT_DEL:
-      return { ...state, contacts: [...state.contacts.filter(contact => contact.id !== action.payload)] }
+      return { ...state, serchContacts: [...state.contacts.filter(contact => contact.id !== action.payload)] }
 
     case contactAT.CONTACT_STATUS:
-      return { ...state, contacts: [...state.contacts.map(contact => contact.id === action.payload ? { ...contact, statusChange: true } : contact)] }
+      return { ...state, serchContacts: [...state.contacts.map(contact => contact.id === action.payload ? { ...contact, statusChange: true } : contact)] }
 
     case contactAT.CONTACT_CHANGE:
       const newContact = [...state.contacts].map(contact => {
@@ -22,8 +24,15 @@ function contactReduser(state = initialState, action) {
         return contact
       }
       })
-
-      return { ...state, contacts: newContact}
+      return { ...state, serchContacts: newContact}
+      
+      case contactAT.CONTACT_SEARCH:
+        const copyContacts = [...state.contacts]
+        const contactFilter = copyContacts.filter(contact => contact.name.slice(0, action.payload.length) === action.payload[0].toUpperCase() + action.payload.slice(1))
+        return {
+          ...state, 
+          serchContacts:contactFilter
+        }
 
     default:
       return state;
